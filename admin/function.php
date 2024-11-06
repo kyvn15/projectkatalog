2084fe19ed825e9443fa0c672f18bf564c464cce
kyvn15 <vanoepepmex@gmail.com>
2024-11-06 15:21:14 +0700

    add

--- admin/function.php
+++ admin/function.php
@@ -11,5 +11,17 @@ function validasiData($data)
     return 0;
 }
 
+function inputbuku ($data , $koneksi) {
+    $judul_buku = $data ['judul'];
+    $kategori = $data ['kategori'];
+    $deskripsi = $data ['deskripsi'];
+    $penerbit = $data ['penerbit'];
+    $tanggal_terbit = $data ['tanggal_terbit'];
+    $isbn = $data ['isbn'];
+    $jumlah_halaman = $data ['jumlah_halaman'];
+    $harga = $data ['harga'];
+    $stok = $data ['stok'];
+}
+
 
 
